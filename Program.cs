c3d5919ee8ab3f11d09b5440a5189ebc49117136
Aseem <aseemanand2@gmail.com>
2020-10-09 21:34:32 +0530

    [Aseem] Added code for toss to decide who will play first

--- Program.cs
+++ Program.cs
@@ -7,37 +7,15 @@ namespace TicTacToeGame
         static void Main(string[] args)
         {
             TicTacToe ticTacToe = new TicTacToe();
-            char[] gameBoard = ticTacToe.createTicTacToeBoard();           
-            char userLetter = ticTacToe.initializeUserLetter();
-            char cpuLetter = ticTacToe.initializeCPULetter(userLetter);
-            while (true)
-            {                
-                Console.WriteLine("Enter your nex move: (1-9)");
-                int playerPos = Convert.ToInt32(Console.ReadLine());
-                while(true)
-                {
-                    if (gameBoard[playerPos]!=' ')
-                    {
-                        Console.WriteLine("Invalid selection!");
-                        playerPos = Convert.ToInt32(Console.ReadLine());
-                    }
-                    else
-                        break;
-                }
-                ticTacToe.moveToDesiredLocation(gameBoard, playerPos, "PLAYER", userLetter);
-                Random random = new Random();
-                int cpuPos = random.Next(1, 10);
-                while (true)
-                {
-                    if (gameBoard[cpuPos] != ' ')
-                    {
-                        cpuPos = random.Next(1, 10);
-                    }
-                    else
-                        break;
-                }                
-                ticTacToe.moveToDesiredLocation(gameBoard,cpuPos, "CPU",cpuLetter);
-                ticTacToe.showBoard(gameBoard);                
+            char[] gameBoard = ticTacToe.createTicTacToeBoard();                      
+            TicTacToe.Player player = ticTacToe.toss();
+            if (player == TicTacToe.Player.USER)
+            {
+                ticTacToe.userMovesFirst(gameBoard);
+            }
+            else if (player == TicTacToe.Player.CPU)
+            {
+                ticTacToe.cpuMovesFirst(gameBoard);
             }
         }
     }
--- TicTacToe.cs
+++ TicTacToe.cs
@@ -6,6 +6,7 @@ namespace TicTacToeGame
 {
     class TicTacToe
     {
+        public enum Player { USER, CPU };
         public char[] createTicTacToeBoard()
         {
             char[] board = new char[10];
@@ -20,7 +21,7 @@ namespace TicTacToeGame
             Console.WriteLine("Enter either X or O whichever you want to play with");
             char userValue = Convert.ToChar(Console.ReadLine().ToUpper());
             while (true)
-            {                
+            {
                 if (userValue != 'X' && userValue != 'O')
                 {
                     Console.WriteLine("Invalid letter input");
@@ -33,7 +34,7 @@ namespace TicTacToeGame
             }
         }
         public char initializeCPULetter(char userValue)
-        {                       
+        {
             char cpuValue = ' ';
             if (userValue == 'X')
                 cpuValue = 'O';
@@ -50,12 +51,107 @@ namespace TicTacToeGame
                     Console.WriteLine("\t--+---+--");
             }
         }
-        public void moveToDesiredLocation(char[] board,int index,string user,char letter)
+        public void moveToDesiredLocation(char[] board, int index, string user, char letter)
         {
             char indexValue = ' ';
-            if (user.Equals("PLAYER") || user.Equals("CPU"))
+            if (user.Equals("USER") || user.Equals("CPU"))
                 indexValue = letter;
             board[index] = indexValue;
+        }
+        public Player toss()
+        {
+            Random random = new Random();
+            Console.WriteLine("Enter\n1-for Heads\n2-for Tails");
+            int userChoice = Convert.ToInt32(Console.ReadLine());
+            int tossValue = random.Next(1, 3);
+            switch (tossValue)
+            {
+                case 1:
+                    Console.WriteLine("Heads it is");
+                    break;
+                case 2:
+                    Console.WriteLine("Tails it is");
+                    break;
+            }
+            if (userChoice == tossValue)
+            {
+                Console.WriteLine("You can go first");
+                return Player.USER;                               
+            }
+            else
+            {
+                Console.WriteLine("Cpu will go first");
+                return Player.CPU;
+            }
+        }
+        public void userMovesFirst(char[] gameBoard)
+        {
+            char userLetter = initializeUserLetter();
+            char cpuLetter = initializeCPULetter(userLetter);
+            while (true)
+            {                
+                Console.WriteLine("Enter your nex move: (1-9)");
+                int playerPos = Convert.ToInt32(Console.ReadLine());
+                while (true)
+                {
+                    if (gameBoard[playerPos] != ' ')
+                    {
+                        Console.WriteLine("Invalid selection!");
+                        playerPos = Convert.ToInt32(Console.ReadLine());
+                    }
+                    else
+                        break;
+                }
+                moveToDesiredLocation(gameBoard, playerPos, "USER", userLetter);
+                Random random = new Random();
+                int cpuPos = random.Next(1, 10);
+                while (true)
+                {
+                    if (gameBoard[cpuPos] != ' ')
+                    {
+                        cpuPos = random.Next(1, 10);
+                    }
+                    else
+                        break;
+                }
+                moveToDesiredLocation(gameBoard, cpuPos, "CPU", cpuLetter);
+                showBoard(gameBoard);
+            }
+            
+        }
+        public void cpuMovesFirst(char[] gameBoard)
+        {
+            char userLetter = initializeUserLetter();
+            char cpuLetter = initializeCPULetter(userLetter);
+            while (true)
+            {                
+                Random random = new Random();
+                int cpuPos = random.Next(1, 10);
+                while (true)
+                {
+                    if (gameBoard[cpuPos] != ' ')
+                    {
+                        cpuPos = random.Next(1, 10);
+                    }
+                    else
+                        break;
+                }
+                moveToDesiredLocation(gameBoard, cpuPos, "CPU", cpuLetter);
+                showBoard(gameBoard);
+                Console.WriteLine("Enter your nex move: (1-9)");
+                int playerPos = Convert.ToInt32(Console.ReadLine());
+                while (true)
+                {
+                    if (gameBoard[playerPos] != ' ')
+                    {
+                        Console.WriteLine("Invalid selection!");
+                        playerPos = Convert.ToInt32(Console.ReadLine());
+                    }
+                    else
+                        break;
+                }
+                moveToDesiredLocation(gameBoard, playerPos, "USER", userLetter);                
+            }            
         }
     }
 }
