92e64e5cdbaaaa0391e75205ed4987bc7e21b9f1
Aseem <aseemanand2@gmail.com>
2020-10-09 17:25:32 +0530

    [Aseem] Created new TicTacToe board

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TicTacToeGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TicTacToe ticTacToe = new TicTacToe();
+            char[] board = ticTacToe.createTicTacToeBoard();            
+        }
+    }
+}
---
+++ TicTacToe.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToeGame
+{
+    class TicTacToe
+    {        
+        public char[] createTicTacToeBoard()
+        {
+            char[] board = new char[10];
+            for (int i = 0; i < board.Length; i++)
+            {
+                board[i] = ' ';
+            }
+            return board;
+        }        
+    }
+}
